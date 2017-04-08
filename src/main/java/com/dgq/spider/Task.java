1664d13b02c4264ac3c227578dd3657148d3dd7e
dengGQ <565820745@qq.com>
2017-04-08 18:11:21 +0800

    qq

---
+++ src/main/java/com/dgq/spider/Task.java
@@ -0,0 +1,16 @@
+package com.dgq.spider;
+
+public class Task implements Runnable{
+	String url = null;
+	
+	public Task(String url) {
+		super();
+		this.url = url;
+	}
+
+	@Override
+	public void run() {
+		
+	}
+	
+}
